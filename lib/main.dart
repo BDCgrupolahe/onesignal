b29419fa3ad09b95406d006256e8ba8de83e45dd
BDCgrupolahe <brandon0629000@gmail.com>
2024-04-09 17:43:58 -0600

    all
    all

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 // import 'package:flutter/foundation.dart';
 // import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
@@ -28,6 +27,7 @@
 //   final GlobalKey webViewKey = GlobalKey();
 //   InAppWebViewController? webViewController;
 //   bool isConnected = true;
+//   int backPressCount = 0;
 
 //   @override
 //   void initState() {
@@ -42,90 +42,100 @@
 //     ));
 
 //     return Scaffold(
-//       body: SafeArea(
-//         child: Column(
-//           children: <Widget>[
-//             Expanded(
-//               child: Stack(
-//                 children: [
-//                   if (isConnected)
-//                     // InAppWebView(
-//                     //   key: webViewKey,
-//                     //   initialUrlRequest: URLRequest(
-//                     //       url: WebUri(
-//                     //           'https://app.congresoneuropediatria.com/')),
-//                     //   initialOptions: InAppWebViewGroupOptions(
-//                     //     android: AndroidInAppWebViewOptions(
-//                     //       useHybridComposition: true,
-//                     //       builtInZoomControls: true,
-//                     //     ),
-//                     //     ios: IOSInAppWebViewOptions(
-//                     //       allowsInlineMediaPlayback: true,
-//                     //     ),
-//                     //     crossPlatform: InAppWebViewOptions(
-//                     //       preferredContentMode: UserPreferredContentMode.DESKTOP,
-//                     //       useShouldOverrideUrlLoading: true,
-//                     //       useOnDownloadStart: true,
-//                     //       useShouldInterceptAjaxRequest: true,
-//                     //     ),
-//                     //     userAgent: 'Mozilla/5.0',
-//                     //     transparentBackground: false,
-//                     //     clearCache: true,
-//                     //     cacheEnabled: false,
-//                     //     clearSessionCache: true,
-//                     //     applicationNameForUserAgent: 'App',
-//                     //     disableContextMenu: true,
-//                     //     useOnLoadResource: true,
-//                     //     useOnDownloadProgress: true,
-//                     //     useShouldInterceptFetchRequest: true,
-//                     //   ),
-//                     //   onWebViewCreated: (controller) {
-//                     //     webViewController = controller;
-//                     //   },
-//                     //   androidOnPermissionRequest:
-//                     //       (controller, origin, resources) async {
-//                     //     return PermissionRequestResponse(
-//                     //       resources: resources,
-//                     //       action: PermissionRequestResponseAction.GRANT,
-//                     //     );
-//                     //   },
-//                     // ),
-//                     InAppWebView(
-//                       key: webViewKey,
-//                       initialUrlRequest: URLRequest(
-//                         url: WebUri('https://app.congresoneuropediatria.com/'),
+//       body: WillPopScope(
+//         onWillPop: onWillPop,
+//         child: SafeArea(
+//           child: Column(
+//             children: <Widget>[
+//               Expanded(
+//                 child: Stack(
+//                   children: [
+//                     if (isConnected)
+//                       InAppWebView(
+//                         key: webViewKey,
+//                         initialUrlRequest: URLRequest(
+//                           url: WebUri(
+//                               'https://app.congresoneuropediatria.com/'),
+//                         ),
+//                         initialOptions: InAppWebViewGroupOptions(
+//                           android: AndroidInAppWebViewOptions(
+//                             useHybridComposition: true,
+//                             builtInZoomControls: true,
+//                           ),
+//                           ios: IOSInAppWebViewOptions(
+//                             allowsInlineMediaPlayback: true,
+//                           ),
+//                           crossPlatform: InAppWebViewOptions(
+//                             preferredContentMode:
+//                                 UserPreferredContentMode.DESKTOP,
+//                           ),
+//                         ),
+//                         onWebViewCreated: (controller) {
+//                           webViewController = controller;
+//                         },
+//                         androidOnPermissionRequest:
+//                             (controller, origin, resources) async {
+//                           return PermissionRequestResponse(
+//                             resources: resources,
+//                             action: PermissionRequestResponseAction.GRANT,
+//                           );
+//                         },
+//                         onLoadStop: (controller, url) {
+//                           // Limpiar el contador de retroceso cuando se carga una nueva página
+//                           backPressCount = 0;
+//                         },
 //                       ),
-//                       initialOptions: InAppWebViewGroupOptions(
-//                         android: AndroidInAppWebViewOptions(
-//                           useHybridComposition: true,
-//                           builtInZoomControls: true,
-//                         ),
-//                         ios: IOSInAppWebViewOptions(
-//                           allowsInlineMediaPlayback: true,
-//                         ),
-//                         crossPlatform: InAppWebViewOptions(
-//                           preferredContentMode:
-//                               UserPreferredContentMode.DESKTOP,
-//                         ),
-//                       ),
-//                       onWebViewCreated: (controller) {
-//                         webViewController = controller;
-//                       },
-//                       androidOnPermissionRequest:
-//                           (controller, origin, resources) async {
-//                         return PermissionRequestResponse(
-//                           resources: resources,
-//                           action: PermissionRequestResponseAction.GRANT,
-//                         );
-//                       },
-//                     ),
-//                 ],
+//                   ],
+//                 ),
 //               ),
-//             ),
-//           ],
+//             ],
+//           ),
 //         ),
 //       ),
 //     );
+//   }
+
+//   Future<bool> onWillPop() async {
+//     if (backPressCount == 1) {
+//       // Si se presiona el botón de atrás dos veces consecutivas, mostrar diálogo de confirmación
+//       bool confirm = await showDialog(
+//         context: context,
+//         builder: (context) => AlertDialog(
+//           title: Text('Salir de la aplicación'),
+//           content: Text('¿Estás seguro de que quieres salir de la aplicación?'),
+//           actions: [
+//             TextButton(
+//               onPressed: () {
+//                 Navigator.pop(context, false);
+//               },
+//               child: Text('Cancelar'),
+//             ),
+//             TextButton(
+//               onPressed: () {
+//                 Navigator.pop(context, true);
+//               },
+//               child: Text('Aceptar'),
+//             ),
+//           ],
+//         ),
+//       );
+//       if (confirm ?? false) {
+//         // Si se acepta, salir de la aplicación
+//         SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+//       }
+//       return false;
+//     } else {
+//       // Intentar retroceder en el WebView
+//       if (await webViewController?.canGoBack() ?? false) {
+//         webViewController?.goBack();
+//       } else {
+//         // Incrementar el contador y esperar 2 segundos antes de resetearlo
+//         backPressCount++;
+//         await Future.delayed(Duration(seconds: 2));
+//         backPressCount = 0;
+//       }
+//       return false;
+//     }
 //   }
 
 //   Future onesignal() async {
@@ -135,7 +145,6 @@
 //     OneSignal.Notifications.requestPermission(true);
 //   }
 
-// // por si no funciona
 //   Future<void> requestPermissions() async {
 //     var cameraStatus = await Permission.camera.request();
 //     var storageStatus = await Permission.storage.request();
@@ -158,8 +167,8 @@
 //         context: context,
 //         builder: (context) => AlertDialog(
 //           title: Text('Conexión a Internet'),
-//           content:
-//               Text('Por favor, conecta a WiFi para acceder a la aplicación.'),
+//           content: Text(
+//               'Por favor, conecta a WiFi para acceder a la aplicación.'),
 //           actions: [
 //             ElevatedButton(
 //               onPressed: () {
@@ -178,6 +187,51 @@
 //   }
 // }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -193,7 +247,9 @@ Future main() async {
       defaultTargetPlatform == TargetPlatform.android) {
     await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
   }
-  runApp(const MaterialApp(home: MyApp()));
+  runApp(const MaterialApp(
+  debugShowCheckedModeBanner: false,  
+  home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -208,11 +264,13 @@ class _MyAppState extends State<MyApp> {
   InAppWebViewController? webViewController;
   bool isConnected = true;
   int backPressCount = 0;
+  bool isLoading = false;
 
   @override
   void initState() {
     super.initState();
     checkConnectivity(); // Verificar la conexión al iniciar la aplicación
+    onesignal(); 
   }
 
   @override
@@ -227,6 +285,7 @@ class _MyAppState extends State<MyApp> {
         child: SafeArea(
           child: Column(
             children: <Widget>[
+              if (isLoading) LinearProgressIndicator(), // Aquí está el nuevo indicador de progreso
               Expanded(
                 child: Stack(
                   children: [
@@ -260,7 +319,15 @@ class _MyAppState extends State<MyApp> {
                             action: PermissionRequestResponseAction.GRANT,
                           );
                         },
+                        onLoadStart: (controller, url) {
+                          setState(() {
+                            isLoading = true; // Comienza la carga, activa el indicador de progreso
+                          });
+                        },
                         onLoadStop: (controller, url) {
+                          setState(() {
+                            isLoading = false; // Termina la carga, desactiva el indicador de progreso
+                          });
                           // Limpiar el contador de retroceso cuando se carga una nueva página
                           backPressCount = 0;
                         },
